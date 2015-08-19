3dc0a0793b0d5f5b049c6c0410d8a9408209f70b
Ethan Durrant <emd519@yahoo.com>
2015-08-18 18:10:07 -0700

    basic functionality of api complete

--- hex_dec_api/hex_dec_api.pl
+++ hex_dec_api/hex_dec_api.pl
@@ -9,7 +9,17 @@ get '/hex-dec/:value' => sub {
     my $type = get_num_type($value);
     return $c->render(text => $type) unless ($type eq 'NaN');
     return $c->render(data => '', status => 400);
-    
+};
+
+post '/hex-dec/:value' => sub {
+    my $c = shift;
+    my $value = $c->stash('value');
+    my $type = get_num_type($value);
+    if($type ne 'NaN'){
+        return $c->render(text => hex($value)) if ($type eq 'HEX');
+        return $c->render(text => sprintf("%x",$value)) if ($type eq 'DEC'); 
+    } 
+    return $c->render(data => '', status => 400);
 };
 
 sub get_num_type {
